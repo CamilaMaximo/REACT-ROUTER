34d30fb468bd643c6bb627237117ba958efa0f73
Camila Maximo <camilamximo20@gmail.com>
2024-09-24 15:03:08 +0100

    tualização

--- router/src/routes/Home.jsx
+++ router/src/routes/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+//6 - carregamento de dados
+import { useEffect, useFetch } from '../hooks/useFetch';
+
+const url = "http://localhost:3000/products";
 
 const Home = () => {
+  const {data: items} = useFetch(url)
   return (
     <div>
         <h1>Home</h1>
